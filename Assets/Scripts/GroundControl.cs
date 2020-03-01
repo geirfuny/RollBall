2239d92572d424d77ce480bb22e9d0e923817553
Geir Furland Nygård <gfurnyg@gmail.com>
2020-03-01 17:50:40 +0100

    Basic gameobjects and materials

---
+++ Assets/Scripts/GroundControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GroundControl : MonoBehaviour
+{
+    private float tiltSideways;
+
+    private float tilSeesaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiltSideways = Input.GetAxis("Horizontal");
+        tilSeesaw = Input.GetAxis("Vertical");
+
+        transform.Rotate(
+            -tilSeesaw * 0.5f,
+            0.0f,
+            tiltSideways * 0.5f
+        );
+    }
+}
